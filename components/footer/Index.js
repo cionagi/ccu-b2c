31bfd1eefa404b74c6875e64a02e84fe9a2ca56a
Luis Zavala <cionagi@hotmail.cl>
2020-05-18 14:38:12 -0400

    ADD Footer component

---
+++ components/footer/Index.js
@@ -0,0 +1,17 @@
+// Dependency
+import React from 'react'
+
+// Components
+import Footer from './Footer'
+import FooterBottom from './FooterBottom'
+
+const Index = () => {
+  return (
+    <footer className="footer">
+      <Footer />
+      <FooterBottom />
+    </footer>
+  )
+}
+
+export default Index
