a8edd43766d241a4fde50ddb430b265b476d3cb8
Luis Zavala <cionagi@hotmail.cl>
2020-05-18 17:27:00 -0400

    UPDATE component Navs

--- components/nav/NavBottom.js
+++ components/nav/NavBottom.js
@@ -1,7 +1,11 @@
 import React from 'react'
 
 const NavBottom = () => {
-  return <div className="nav-bottom">BOTTOM</div>
+  return (
+    <div className="nav-bottom d-flex justify-content-center align-items-center">
+      <span className="text-center py-2">Tu categoría de cliente es: Copero | Disfruta de más de 100 marcas.</span>
+    </div>
+  )
 }
 
 export default NavBottom
