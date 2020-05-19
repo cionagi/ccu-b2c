c93f80c597125287dc40f1ea1530f14c5994f96b
Luis Zavala <cionagi@hotmail.cl>
2020-05-19 10:33:54 -0400

    ADD ProductCard component

--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,30 @@ import React from 'react'
 // Components
 import Nav from '../components/nav/Index'
 import Footer from '../components/footer/Index'
+import ProductCard from '../components/productCard/Index'
 
 export default function Home() {
   return (
     <>
       <Nav />
+      <main>
+        <div className="container">
+          <div className="row">
+            <div className="col-4">
+              <ProductCard />
+            </div>
+            <div className="col-4">
+              <ProductCard />
+            </div>
+            <div className="col-4">
+              <ProductCard />
+            </div>
+            <div className="col-4">
+              <ProductCard />
+            </div>
+          </div>
+        </div>
+      </main>
       <Footer />
     </>
   )
