6dd66441e77d8a38614a0f81c21040b714e331b0
Luis Zavala <cionagi@hotmail.cl>
2020-05-19 11:29:45 -0400

    UPDATE show image in productCard

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,18 @@ import React from 'react'
 import Nav from '../components/nav/Index'
 import Footer from '../components/footer/Index'
 import ProductCard from '../components/productCard/Index'
+import Title from '../components/Title'
+
+// TODO remove
+const PRODUCT_FAKE = {
+  name: 'ESCUDO',
+  description: '24 botellas de 355 cc',
+  offer: '40%',
+  price: '$9.990',
+  priceRef: '$16.600',
+  imgSrc: 'https://www.labarra.cl/481-large_default/escudo.jpg',
+  quantityStock: '100',
+}
 
 export default function Home() {
   return (
@@ -12,18 +24,21 @@ export default function Home() {
       <Nav />
       <main>
         <div className="container">
+          <section>
+            <Title title="Lorem ipsum dolor sit amet " />
+          </section>
           <div className="row">
             <div className="col-4">
-              <ProductCard />
+              <ProductCard product={PRODUCT_FAKE} />
             </div>
             <div className="col-4">
-              <ProductCard />
+              <ProductCard product={PRODUCT_FAKE} />
             </div>
             <div className="col-4">
-              <ProductCard />
+              <ProductCard product={PRODUCT_FAKE} />
             </div>
             <div className="col-4">
-              <ProductCard />
+              <ProductCard product={PRODUCT_FAKE} />
             </div>
           </div>
         </div>
